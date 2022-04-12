aaf72e1c21832ffafa3d638be46a38569153168e
jiyoon3421 <jiyoon3421@gmail.com>
2022-04-13 08:03:38 +0900

    context api공부

---
+++ context-tutorial/src/components/SelectColors.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { ColorConsumer } from "../contexts/color";
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+const SelectColors = () => {
+  return (
+    <div>
+      <h2>생상을 선택하세요.</h2>
+      {/* <div style={{ display: "flex" }}>
+        {colors.map((color) => (
+          <div
+            key={color}
+            style={{
+              background: color,
+              width: "24px",
+              height: "24px",
+              cursor: "pointer",
+            }}
+          />
+        ))}
+      </div> */}
+      <ColorConsumer>
+        {({ actions }) => (
+          <div style={{ display: "flex" }}>
+            {colors.map((color) => (
+              <div
+                key={color}
+                style={{
+                  background: color,
+                  width: "24px",
+                  height: "24px",
+                  cursor: "pointer",
+                }}
+                onClick={() => actions.setColor(color)}
+              />
+            ))}
+          </div>
+        )}
+      </ColorConsumer>
+    </div>
+  );
+};
+
+export default SelectColors;
